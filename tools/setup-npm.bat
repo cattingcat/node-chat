33b77832cdd43f8e03cb7e5f8d9d46307528d8b6
Mark Martynov <mark_martynov@epam.com>
2015-02-12 12:17:03 +0400

    fix

--- tools/setup-npm.bat
+++ tools/setup-npm.bat
@@ -2,7 +2,7 @@ WHERE npm
 IF %ERRORLEVEL% NEQ 0 (
 	ECHO npm is not exist, please install node
 ) ELSE (
-	call npm install -g grunt-cli
+	call npm install -g gulp
 	cd ..\src
 	call npm install
 )	
